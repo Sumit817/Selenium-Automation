78d8bf8f3602577e57129cf5dd567fa6153aec81
Sumit817 <59187741+Sumit817@users.noreply.github.com>
2020-01-04 15:18:51 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,30 @@
+/*
+ * 
+ * @Sumit - Java class to add or subtract 2 numbers
+ */
+
+package assignment2;
+import java.util.Scanner;
+
+
+public class Operators {
+
+	public static void main(String[] args) {
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("Please enter the first number");
+		int num1 = scan2.nextInt();
+		System.out.println("Please enter the second number");
+		int num2 = scan2.nextInt();
+		
+		Operators.add1(num1, num2);
+		scan2.close();
+	}
+	public static void add1(int num3, int num4) {
+		int sum = num3 + num4;
+		System.out.println("Sum of two number is: "+ sum);
+		
+		
+	}
+	
+	
+}
